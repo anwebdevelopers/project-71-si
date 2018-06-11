b0bc8579e0148567dc007217e8dc92da2fab2a45
anwebdevelopers <anwebdev78@gmail.com>
2018-06-11 19:51:18 +0300

    11.06.2018 20:10

--- html/js/common.js
+++ html/js/common.js
@@ -63,4 +63,68 @@ $(function() {
             }
         }
     });
+
+    /*******************************************************/
+    //SLIDER
+    /*******************************************************/
+    $('.slider').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        nav: true,
+        navText: '',
+        autoplayTimeout: 10000,
+        autoplay: true,
+        smartSpeed: 600
+    });
+
+    /*******************************************************/
+    //POPUP
+    /*******************************************************/
+
+    $('.popup-with-move-anim').magnificPopup({
+       type: 'inline',
+       fixedContentPos: false,
+       fixedBgPos: true,
+       overflowY: 'auto',
+       closeBtnInside: true,
+       preloader: false,
+       midClick: true,
+       removalDelay: 300,
+       mainClass: 'my-mfp-slide-bottom'
+    });
+
+    //------------------------------------------------------------
+    // Google Map
+    //------------------------------------------------------------
+    var markerPositions = new Array();
+    	markerPositions[1] = new google.maps.LatLng(55.817883, 37.440213);
+
+    function initialize() {
+        var loc, map;
+
+        loc = new google.maps.LatLng(55.817883, 37.440213);
+
+        map = new google.maps.Map(document.getElementById('map'), {
+             zoom: 15,
+             center: loc,
+             mapTypeId: google.maps.MapTypeId.ROADMAP,
+             scrollwheel: false
+        });
+
+        var iconlink = 'img/';
+        for(var i in markerPositions) {
+          var markerPosition = markerPositions[i];
+
+          var marker = new google.maps.Marker({
+              map: map,
+              position: markerPosition,
+              visible: true,
+              icon: iconlink + 'icon-map.png'
+          });
+        }
+
+        map.set('styles', [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ccc"},{"lightness":20},{"saturation":-97}]}]);
+    }
+    initialize();
+    google.maps.event.addDomListener(window, 'load', initialize);
 });
